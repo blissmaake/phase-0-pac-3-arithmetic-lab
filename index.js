75ef3fa85e45dced9ff9022d1f77c0e6d804b542
Bliss <blissmaake140@gmail.com>
2022-11-21 12:05:01 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+    return a + b
+}
+
+function subtract(b, a){
+    return b - a
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(b, a){
+    return b / a
+}
+
+function increment(a){
+    return ++a
+}
+
+function decrement(a){
+    return --a
+}
+
+function makeInt(n) {
+    parseInt(n, 10);
+return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    parseFloat(n);
+return parseFloat(n)
+}
+
